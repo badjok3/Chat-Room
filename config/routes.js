a60d035e97ad94e86b98c634558ec47e5f6cc7f7
badjok3 <dani.popoff@abv.bg>
2017-09-10 23:36:50 +0300

    fixed minor mistakes

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,11 @@
 const userController = require('./../controllers/user');
 const homeController = require('./../controllers/home');
-const io = require('socket.io').listen(80);
 const chatController = require('./../controllers/chat');
 
 module.exports = (app) => {
     app.get('/', homeController.index);
 
-    app.get('/chatRoom/allChats', chatController.chatGet);
+    // app.get('/chatRoom/allChats', chatController.chatGet);
 
     app.get('/user/register', userController.registerGet);
     app.post('/user/register', userController.registerPost);
