1b47dea3d1e92c156eae9e894b552e421c3eb559
badjok3 <dani.popoff@abv.bg>
2017-09-13 15:53:23 +0300

    fixed message submit in chat

--- controllers/message.js
+++ controllers/message.js
@@ -1,16 +1,36 @@
 const Conversation = require('../models/Conversation'),
     Message = require('../models/Message'),
     User = require('mongoose').model('User');
-
+const FormatDate = require('../utilities/formatDate');
 
 exports.postMessage = function (req, res) {
 
-    let message = new Message ({
+    let message = new Message({
         conversationId: req.body.inputConversationId,
         author: req.user,
         body: req.body.message
     });
 
     message.save();
+
+    Conversation.find({})
+        .then(conversations => {
+
+            Conversation.findOne({'_id': req.body.inputConversationId})
+                .populate('participants')
+                .then(conversation => {
+
+                    Message.find({'conversationId': conversation._id})
+                        .populate('author')
+                        .then(messages => {
+                            messages.forEach(m => m.date = FormatDate.formatDate(m.timestamps));
+                            res.render('conversation/conversation', {
+                                conversations: conversations,
+                                conversation: conversation,
+                                messages: messages
+                            })
+                        })
+                })
+        });
 };
 
